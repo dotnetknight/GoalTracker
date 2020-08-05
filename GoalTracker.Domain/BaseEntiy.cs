d701b7b1682c7b7b27332bde7b768c3e6e2542a5
Nick Qantaria <skott56@outlook.com>
2020-08-05 17:03:44 +0400

    back-end :  added user class, mapper and entity builder in domain layer

---
+++ GoalTracker.Domain/BaseEntiy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GoalTracker.Domain
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime AddedDate { get; set; }
+    }
+}
---
+++ GoalTracker.Domain/User/UserMaper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GoalTracker.Domain.User
+{
+    public class UserMap
+    {
+        public UserMap(EntityTypeBuilder<User> entityBuilder)
+        {
+            entityBuilder.HasKey(t => t.Id);
+            entityBuilder.Property(t => t.Email).IsRequired();
+            entityBuilder.Property(t => t.FirstName).IsRequired();
+            entityBuilder.Property(t => t.LastName).IsRequired();
+            entityBuilder.Property(t => t.Password).IsRequired();
+        }
+    }
+}
